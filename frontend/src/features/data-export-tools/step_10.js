7dbe4434682b7e469227dd8166454e9b4f101cee
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-23 22:34:16 +0100

    chore(data-export-tools): update dependencies and minor tweaks

---
+++ frontend/src/features/data-export-tools/step_10.js
@@ -0,0 +1,4 @@
+// chore: update dependencies and minor tweaks for data-export-tools
+export const data_export_tools_step_10 = () => {
+  console.log('Executing update dependencies and minor tweaks');
+};
