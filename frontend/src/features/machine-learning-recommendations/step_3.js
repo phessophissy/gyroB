0711e149228c775e97bb0c74f99f25ca7b006082
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-23 22:40:02 +0100

    test(machine-learning-recommendations): add unit tests for component

---
+++ frontend/src/features/machine-learning-recommendations/step_3.js
@@ -0,0 +1,4 @@
+// test: add unit tests for component for machine-learning-recommendations
+export const machine_learning_recommendations_step_3 = () => {
+  console.log('Executing add unit tests for component');
+};
