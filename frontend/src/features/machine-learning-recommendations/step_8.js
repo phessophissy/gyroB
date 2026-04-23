9a68ea75bcf973ee622e906def39923d23e28b7f
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-23 22:40:02 +0100

    test(machine-learning-recommendations): add integration tests

---
+++ frontend/src/features/machine-learning-recommendations/step_8.js
@@ -0,0 +1,4 @@
+// test: add integration tests for machine-learning-recommendations
+export const machine_learning_recommendations_step_8 = () => {
+  console.log('Executing add integration tests');
+};
