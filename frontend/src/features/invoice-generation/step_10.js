3e545fc6769b660998680c6ac809526c708a35b2
phessophissy <73696997+phessophissy@users.noreply.github.com>
2026-04-23 22:38:12 +0100

    chore(invoice-generation): update dependencies and minor tweaks

---
+++ frontend/src/features/invoice-generation/step_10.js
@@ -0,0 +1,4 @@
+// chore: update dependencies and minor tweaks for invoice-generation
+export const invoice_generation_step_10 = () => {
+  console.log('Executing update dependencies and minor tweaks');
+};
