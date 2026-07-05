fa9407dd6ba6338cc054387358a63d052c09fac4
phessophissy <adefesobi@yahoo.com>
2026-07-06 00:22:03 +0100

    test: fix AlreadyPlayed error case (RoundFull is unreachable after auto-finalize)

--- test/GyroBoard.errors.t.js
+++ test/GyroBoard.errors.t.js
@@ -5,8 +5,8 @@ const { ethers } = hardhat;
 const parse = ethers.parseUnits;
 
 describe("GyroBoard error cases", function () {
-  it("reverts RoundFull when eleventh player joins", async function () {
-    const [creator, ...players] = await ethers.getSigners();
+  it("reverts AlreadyPlayed when the same wallet plays twice in one round", async function () {
+    const [creator, player] = await ethers.getSigners();
     const token = await (await ethers.getContractFactory("MockUSDm")).deploy();
     const board = await (await ethers.getContractFactory("GyroBoard")).deploy(
       await token.getAddress(),
@@ -14,14 +14,10 @@ describe("GyroBoard error cases", function () {
     );
 
     await board.createRoom(3, parse("1", 18));
-    for (let i = 0; i < 10; i++) {
-      await token.mint(players[i].address, parse("50", 18));
-      await token.connect(players[i]).approve(await board.getAddress(), parse("1", 18));
-      await board.connect(players[i]).play(3, 1);
-    }
+    await token.mint(player.address, parse("50", 18));
+    await token.connect(player).approve(await board.getAddress(), parse("1", 18));
+    await board.connect(player).play(3, 5);
 
-    await token.mint(players[10].address, parse("50", 18));
-    await token.connect(players[10]).approve(await board.getAddress(), parse("1", 18));
-    await expect(board.connect(players[10]).play(3, 2)).to.be.revertedWithCustomError(board, "RoundFull");
+    await expect(board.connect(player).play(3, 6)).to.be.revertedWithCustomError(board, "AlreadyPlayed");
   });
 });
